7745000f3553b6d18c92052c027c497796e9505c
Tony Lima <mantony0148@icloud.com>
2023-01-12 18:10:30 -0300

    Update Regex.js

--- src/utils/Regex.js
+++ src/utils/Regex.js
@@ -6,7 +6,8 @@ const Regex = () =>
     /\s*(select|SELECT)\s*([a-zA-Z,*_.\s]+)(from|FROM)\s*([a-zA-Z]+)\s*(left|LEFT|inner|INNER|right|RIGHT)\s*(join|JOIN)\s*([a-zA-Z,*_\s]+)\s*(on)\s*([a-zA-Z,*_\s]+)\.([a-zA-Z,*_\s]+)\s*=\s*([a-zA-Z,*_\s]+)\.([a-zA-Z,*_\s]+)\s*$/g,
     /\s*(select|SELECT)\s*([a-zA-Z,*_.\s]+)(from|FROM)\s*([a-zA-Z]+)\s*(left|LEFT|inner|INNER|right|RIGHT)\s*(join|JOIN)\s*([a-zA-Z,*_\s]+)\s*(on)\s*([a-zA-Z,*_\s]+)\.([a-zA-Z,*_\s]+)\s*=\s*([a-zA-Z,*_\s]+)\.([a-zA-Z,*_\s]+)\s*(where|WHERE)\s*(.*)(\s^\+|-|\*|\/|=|>|<|>=|&|\||%|!|\(|\))\s*(\d*)\s*$/g,
     /\s*(create table)\s*([a-zA-Z]+)\s*\(\s*([a-zA-Z_\s*,(0-9)]+)\s*constraint\s*([a-zA-Z_]+)\s*primary\s*key\(\s*([a-zA-Z_\s*,]+)\s*\)\s*\)/g,
-    /\s*(create table)\s*([a-zA-Z]+)\s*\(\s*([a-zA-Z_\s*,(0-9)]+)\s*constraint\s*([a-zA-Z_]+)\s*primary\s*key\(\s*([a-zA-Z_\s*,]+)\s*\)\s*,\s*constraint\s*([a-zA-Z_]+)\s*foreign\s*key\(\s*([a-zA-Z_]+)\)\s*references\s*([a-zA-Z_]+)\s*\)/g
+    /\s*(create table)\s*([a-zA-Z]+)\s*\(\s*([a-zA-Z_\s*,(0-9)]+)\s*constraint\s*([a-zA-Z_]+)\s*primary\s*key\(\s*([a-zA-Z_\s*,]+)\s*\)\s*,\s*constraint\s*([a-zA-Z_]+)\s*foreign\s*key\(\s*([a-zA-Z_]+)\)\s*references\s*([a-zA-Z_]+)\s*\)/g,
+    /^[0-9]+-[a-zA-Z-]+$|^[0-9]+$/
   ]
 
 export const createTableSubRegex = /\s*([a-zA-Z_]+)\s*(varchar|int|decimal)?\(?([0-9\s*,]+)?\)?([not\s*null]+)?\s*,/gm
